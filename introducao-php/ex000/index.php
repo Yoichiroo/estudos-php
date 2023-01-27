e700db6eeb2b1c1629b4abd1a421904d45aa39b5
Lucas Torres <repositoriolucas@gmail.com>
2023-01-27 15:32:49 -0300

    Primeiros passos em PHP

---
+++ introducao-php/ex000/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exercício de php</title>
+    <style>
+        * {
+            font-family: "Poppins";
+        }
+        p {
+            font-size: 1.2em;
+        }
+    </style>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Olá, mundo! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Se livrando da maldição</p>
+</body>
+</html>
