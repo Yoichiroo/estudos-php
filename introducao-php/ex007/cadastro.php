9cc0c2c575c0baf1107c92162faabdec41f137d8
Lucas Torres <repositoriolucas@gmail.com>
2023-03-12 12:18:31 -0300

    Entendendo melhor os métodos de formulário

--- introducao-php/ex007/cadastro.php
+++ introducao-php/ex007/cadastro.php
@@ -8,8 +8,19 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php 
-        
-    ?>
+    <header>
+        <h1>Resultado do Processamento</h1>
+    </header>
+    <main>
+        <?php
+            /* Conteúdo de uma variável superglobal
+                var_dump($_GET);
+                var_dump($_POST);
+                var_dump($_REQUEST); // Junção de $_GET, $_POST, $_COOKIES
+            */
+            $nome = $_REQUEST["nome"];
+            $sobrenome = $_REQUEST["sobrenome"];
+        ?>
+    </main>
 </body>
 </html>
