d22f555eb555b99f9a5c01c99c6789a989c27f2f
Lucas Torres <repositoriolucas@gmail.com>
2023-04-16 14:15:19 -0300

    Desafio de número aleatório

--- introducao-php/desafios/d002/index.php
+++ introducao-php/desafios/d002/index.php
@@ -8,6 +8,23 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <?php 
+        $min = 10;
+        $max = 100;
+        $num = mt_rand($min, $max);
+    ?>
+    <main>
+        <h1>Trabalhando com números aleatórios</h1>
+        <?php 
+            echo "<p>Gerando um número aleatório entre $min e $max...</p>";
+        ?>
+        <button onclick="javascript:document.location.reload()">&#x1F504;Gerar outro valor</button>
+    </main>
+    <section>
+        <h2>Resultado final</h2>
+        <?php 
+            echo "<p>O número gerado foi <strong>$num</strong></p>";
+        ?>
+    </section>
 </body>
 </html>
